34c8ca62601a5a08a8977800d4b195ab67e1f74c
Ben Gourley <bn@grly.me>
2019-10-18 14:07:12 +0100

    Test inline script tracking

--- packages/plugin-inline-script-content/test/inline-script-content.test.js
+++ packages/plugin-inline-script-content/test/inline-script-content.test.js
@@ -3,11 +3,11 @@ const { describe, it, expect, spyOn } = global
 const plugin = require('../')
 
 const Client = require('@bugsnag/core/client')
-const Report = require('@bugsnag/core/report')
+const Event = require('@bugsnag/core/event')
 const VALID_NOTIFIER = { name: 't', version: '0', url: 'http://' }
 
 describe('plugin: inline script content', () => {
-  it('should add a beforeSend callback which captures the HTML content if file=current url', () => {
+  it('should add a beforeSend callback which captures the HTML content if file=current url', done => {
     const scriptContent = `function BadThing() {
   Error.apply(this, args)
 }
@@ -29,30 +29,33 @@ Lorem ipsum dolor sit amet.
     }
     const window = { location: { href: 'https://app.bugsnag.com/errors' } }
 
-    const client = new Client(VALID_NOTIFIER)
+    const client = new Client({ apiKey: 'API_KEY_YEAH' }, undefined, VALID_NOTIFIER)
     const payloads = []
-    client.setOptions({ apiKey: 'API_KEY_YEAH' })
-    client.configure()
     client.use(plugin, document, window)
 
-    expect(client.config.beforeSend.length).toBe(1)
-    client.delivery(client => ({ sendReport: (payload) => payloads.push(payload) }))
-    client.notify(new Report('BadThing', 'Happens in script tags', [
+    expect(client._callbacks.onError.length).toBe(1)
+    client._delivery(client => ({
+      sendEvent: (payload, cb) => {
+        payloads.push(payload)
+        cb()
+      }
+    }))
+    client._notify(new Event('BadThing', 'Happens in script tags', [
       { fileName: window.location.href, lineNumber: 10 }
-    ]))
-    expect(payloads.length).toEqual(1)
-    expect(payloads[0].events[0].stacktrace[0].code).toBeDefined()
-    expect(payloads[0].events[0].metaData.script).toBeDefined()
-    expect(payloads[0].events[0].metaData.script.content).toEqual(scriptContent)
+    ]), () => {}, () => {
+      expect(payloads.length).toEqual(1)
+      expect(payloads[0].events[0].errors[0].stacktrace[0].code).toBeDefined()
+      expect(payloads[0].events[0]._metadata.script).toBeDefined()
+      expect(payloads[0].events[0]._metadata.script.content).toEqual(scriptContent)
+      done()
+    })
   })
 
   it('calls the previous onreadystatechange handler if it exists', done => {
     const prevHandler = () => { done() }
     const document = { documentElement: { outerHTML: '' }, onreadystatechange: prevHandler }
     const window = { location: { href: 'https://app.bugsnag.com/errors' }, document }
-    const client = new Client(VALID_NOTIFIER)
-    client.setOptions({ apiKey: 'API_KEY_YEAH' })
-    client.configure()
+    const client = new Client({ apiKey: 'API_KEY_YEAH' }, undefined, VALID_NOTIFIER)
     client.use(plugin, document, window)
     // check it installed a new onreadystatechange handler
     expect(document.onreadystatechange === prevHandler).toBe(false)
@@ -67,15 +70,13 @@ Lorem ipsum dolor sit amet.
     function EventTarget () {}
     EventTarget.prototype.addEventListener = addEventListener
     window.EventTarget = EventTarget
-    const client = new Client(VALID_NOTIFIER)
-    client.setOptions({ apiKey: 'API_KEY_YEAH', trackInlineScripts: false })
-    client.configure()
+    const client = new Client({ apiKey: 'API_KEY_YEAH', trackInlineScripts: false }, undefined, VALID_NOTIFIER)
     client.use(plugin, document, window)
     // check the addEventListener function was not wrapped
     expect(window.EventTarget.prototype.addEventListener).toBe(addEventListener)
   })
 
-  it('truncates script content to a reasonable length', () => {
+  it('truncates script content to a reasonable length', done => {
     let scriptContent = ``
     for (let i = 0; i < 10000; i++) {
       scriptContent += `function fn_${i} (arg0, arg1, arg2) {\n`
@@ -99,24 +100,29 @@ Lorem ipsum dolor sit amet.
     }
     const window = { location: { href: 'https://app.bugsnag.com/errors' } }
 
-    const client = new Client(VALID_NOTIFIER)
+    const client = new Client({ apiKey: 'API_KEY_YEAH' }, undefined, VALID_NOTIFIER)
     const payloads = []
-    client.setOptions({ apiKey: 'API_KEY_YEAH' })
-    client.configure()
     client.use(plugin, document, window)
 
-    expect(client.config.beforeSend.length).toBe(1)
-    client.delivery(client => ({ sendReport: (payload) => payloads.push(payload) }))
-    client.notify(new Report('BadThing', 'Happens in script tags', [
+    expect(client._callbacks.onError.length).toBe(1)
+    client._delivery(client => ({
+      sendEvent: (payload, cb) => {
+        payloads.push(payload)
+        cb()
+      }
+    }))
+    client._notify(new Event('BadThing', 'Happens in script tags', [
       { fileName: window.location.href, lineNumber: 10 }
-    ]))
-    expect(payloads.length).toEqual(1)
-    expect(payloads[0].events[0].stacktrace[0].code).toBeDefined()
-    expect(payloads[0].events[0].metaData.script).toBeDefined()
-    expect(payloads[0].events[0].metaData.script.content.length).toBe(500000)
+    ]), () => {}, () => {
+      expect(payloads.length).toEqual(1)
+      expect(payloads[0].events[0].errors[0].stacktrace[0].code).toBeDefined()
+      expect(payloads[0].events[0]._metadata.script).toBeDefined()
+      expect(payloads[0].events[0]._metadata.script.content.length).toBe(500000)
+      done()
+    })
   })
 
-  it('truncates surrounding code lines to a reasonable length', () => {
+  it('truncates surrounding code lines to a reasonable length', done => {
     const longMessage = Array(1000).fill('jim').join(',')
     const scriptContent = `function fn (arg0, arg1, arg2) {
   console.log('${longMessage}')
@@ -138,27 +144,32 @@ Lorem ipsum dolor sit amet.
     }
     const window = { location: { href: 'https://app.bugsnag.com/errors' } }
 
-    const client = new Client(VALID_NOTIFIER)
+    const client = new Client({ apiKey: 'API_KEY_YEAH' }, undefined, VALID_NOTIFIER)
     const payloads = []
-    client.setOptions({ apiKey: 'API_KEY_YEAH' })
-    client.configure()
     client.use(plugin, document, window)
 
-    expect(client.config.beforeSend.length).toBe(1)
-    client.delivery(client => ({ sendReport: (payload) => payloads.push(payload) }))
-    client.notify(new Report('BadThing', 'Happens in script tags', [
+    expect(client._callbacks.onError.length).toBe(1)
+    client._delivery(client => ({
+      sendEvent: (payload, cb) => {
+        payloads.push(payload)
+        cb()
+      }
+    }))
+    client._notify(new Event('BadThing', 'Happens in script tags', [
       { fileName: window.location.href, lineNumber: 7 }
-    ]))
-    expect(payloads.length).toEqual(1)
-    expect(payloads[0].events[0].stacktrace[0].code).toBeDefined()
-    const surroundingCode = payloads[0].events[0].stacktrace[0].code
-    Object.keys(surroundingCode).forEach(line => {
-      expect(surroundingCode[line].length > 200).toBe(false)
+    ]), () => {}, () => {
+      expect(payloads.length).toEqual(1)
+      expect(payloads[0].events[0].errors[0].stacktrace[0].code).toBeDefined()
+      const surroundingCode = payloads[0].events[0].errors[0].stacktrace[0].code
+      Object.keys(surroundingCode).forEach(line => {
+        expect(surroundingCode[line].length > 200).toBe(false)
+      })
+      expect(payloads[0].events[0]._metadata.script).toBeDefined()
+      done()
     })
-    expect(payloads[0].events[0].metaData.script).toBeDefined()
   })
 
-  it('works when the stacktrace is empty', () => {
+  it('works when the stacktrace is empty', done => {
     const scriptContent = `console.log("EMPTY")`
     const document = {
       scripts: [ { innerHTML: scriptContent } ],
@@ -176,19 +187,24 @@ Lorem ipsum dolor sit amet.
     }
     const window = { location: { href: 'https://app.bugsnag.com/errors' } }
 
-    const client = new Client(VALID_NOTIFIER)
+    const client = new Client({ apiKey: 'API_KEY_YEAH' }, undefined, VALID_NOTIFIER)
     const payloads = []
-    client.setOptions({ apiKey: 'API_KEY_YEAH' })
-    client.configure()
     client.use(plugin, document, window)
 
-    expect(client.config.beforeSend.length).toBe(1)
-    client.delivery(client => ({ sendReport: (payload) => payloads.push(payload) }))
+    expect(client._callbacks.onError.length).toBe(1)
+    client._delivery(client => ({
+      sendEvent: (payload, cb) => {
+        payloads.push(payload)
+        cb()
+      }
+    }))
     const spy = spyOn(client.__logger, 'error')
-    client.notify(new Report('EmptyStacktrace', 'Has nothing in it', []))
-    expect(payloads.length).toEqual(1)
-    expect(payloads[0].events[0].stacktrace).toEqual([])
-    expect(spy).toHaveBeenCalledTimes(0)
+    client._notify(new Event('EmptyStacktrace', 'Has nothing in it', []), () => {}, () => {
+      expect(payloads.length).toEqual(1)
+      expect(payloads[0].events[0].errors[0].stacktrace).toEqual([])
+      expect(spy).toHaveBeenCalledTimes(0)
+      done()
+    })
   })
 
   it('calls removeEventListener with wrapped and unwrapped callback', () => {
@@ -223,16 +239,14 @@ Lorem ipsum dolor sit amet.
     window.addEventListener('click', myfun)
 
     const spy = spyOn(Window.prototype, 'removeEventListener')
-    const client = new Client(VALID_NOTIFIER)
-    client.setOptions({ apiKey: 'API_KEY_YEAH' })
-    client.configure()
+    const client = new Client({ apiKey: 'API_KEY_YEAH' }, undefined, VALID_NOTIFIER)
     client.use(plugin, document, window)
 
     window.removeEventListener('click', myfun)
     expect(spy).toHaveBeenCalledTimes(2)
   })
 
-  it('gets the correct line numbers for errors at the start of the document', () => {
+  it('gets the correct line numbers for errors at the start of the document', done => {
     const scriptContent = `throw new Error('oh')\nconsole.log('next')`
     const document = {
       scripts: [ { innerHTML: scriptContent } ],
@@ -243,24 +257,29 @@ Lorem ipsum dolor sit amet.
     }
     const window = { location: { href: 'https://app.bugsnag.com/errors' } }
 
-    const client = new Client(VALID_NOTIFIER)
+    const client = new Client({ apiKey: 'API_KEY_YEAH' }, undefined, VALID_NOTIFIER)
     const payloads = []
-    client.setOptions({ apiKey: 'API_KEY_YEAH' })
-    client.configure()
     client.use(plugin, document, window)
 
-    expect(client.config.beforeSend.length).toBe(1)
-    client.delivery(client => ({ sendReport: (payload) => payloads.push(payload) }))
-    client.notify(new Report('Error', 'oh', [
+    expect(client._callbacks.onError.length).toBe(1)
+    client._delivery(client => ({
+      sendEvent: (payload, cb) => {
+        payloads.push(payload)
+        cb()
+      }
+    }))
+    client._notify(new Event('Error', 'oh', [
       { fileName: window.location.href, lineNumber: 1 }
-    ]))
-    expect(payloads.length).toEqual(1)
-    expect(payloads[0].events[0].stacktrace[0].code).toEqual({
-      1: '<!-- DOC START -->',
-      2: '<script>throw new Error(\'oh\')',
-      3: 'console.log(\'next\')</script>'
+    ]), () => {}, () => {
+      expect(payloads.length).toEqual(1)
+      expect(payloads[0].events[0].errors[0].stacktrace[0].code).toEqual({
+        1: '<!-- DOC START -->',
+        2: '<script>throw new Error(\'oh\')',
+        3: 'console.log(\'next\')</script>'
+      })
+      expect(payloads[0].events[0]._metadata.script).toBeDefined()
+      expect(payloads[0].events[0]._metadata.script.content).toEqual(scriptContent)
+      done()
     })
-    expect(payloads[0].events[0].metaData.script).toBeDefined()
-    expect(payloads[0].events[0].metaData.script.content).toEqual(scriptContent)
   })
 })
