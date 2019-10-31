88075477bf3c381a1c022ff267ea583a01e9f182
Ben Gourley <bn@grly.me>
2019-10-31 16:14:50 +0000

    Lint fix

--- packages/node/src/test/config-loader.test.js
+++ packages/node/src/test/config-loader.test.js
@@ -6,14 +6,14 @@ const { execSync } = require('child_process')
 describe('config loader', () => {
   describe('loadConfig()', () => {
     it('works with process cwd', () => {
-      const str = execSync(`node app.js`, { cwd: `${__dirname}/fixtures/config-loader`, encoding: 'utf8' })
+      const str = execSync('node app.js', { cwd: `${__dirname}/fixtures/config-loader`, encoding: 'utf8' })
       const config = JSON.parse(str)
       expect(config.apiKey).toBe('123')
       expect(config.releaseStage).toBe('jim')
     })
 
     it('works with app entrypoint', () => {
-      const str = execSync(`node alt/entrypoint.js`, { cwd: `${__dirname}/fixtures/config-loader`, encoding: 'utf8' })
+      const str = execSync('node alt/entrypoint.js', { cwd: `${__dirname}/fixtures/config-loader`, encoding: 'utf8' })
       const config = JSON.parse(str)
       expect(config.apiKey).toBe('123')
       expect(config.releaseStage).toBe('jim')
