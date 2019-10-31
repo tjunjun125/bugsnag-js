b7ba95175605422b65ee2e26aa79ddfd7df3054a
Ben Gourley <bn@grly.me>
2019-10-31 14:43:46 +0000

    Remove Bugsnag* prefix from class names

--- packages/core/event.js
+++ packages/core/event.js
@@ -5,7 +5,7 @@ const { reduce, filter, map } = require('./lib/es-utils')
 const jsRuntime = require('./lib/js-runtime')
 const metadataDelegate = require('./lib/metadata-delegate')
 
-class BugsnagEvent {
+class Event {
   constructor (errorClass, errorMessage, stacktrace = [], originalError, handledState = defaultHandledState()) {
     this.apiKey = undefined
     this.context = undefined
@@ -129,7 +129,7 @@ const stringOrFallback = (str, fallback) => typeof str === 'string' && str ? str
 
 // Helpers
 
-BugsnagEvent.getStacktrace = function (error, errorFramesToSkip = 0, generatedFramesToSkip = 0) {
+Event.getStacktrace = function (error, errorFramesToSkip = 0, generatedFramesToSkip = 0) {
   if (hasStack(error)) return ErrorStackParser.parse(error).slice(errorFramesToSkip)
   // in IE11 a new Error() doesn't have a stacktrace until you throw it, so try that here
   try {
@@ -147,4 +147,4 @@ BugsnagEvent.getStacktrace = function (error, errorFramesToSkip = 0, generatedFr
   }
 }
 
-module.exports = BugsnagEvent
+module.exports = Event
